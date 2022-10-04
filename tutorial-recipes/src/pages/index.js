fbdc308e4f6663c8b1d1aaf093eb39149f7e8088
Daniel <itzdanielmichael@gmail.com>
2022-10-04 13:28:23 -0400

    rename dir

--- tutorial-recipes/src/pages/index.js
+++ tutorial-recipes/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../layouts/Layout/Layout';
+import Images from './components/images';
 
 export default function Home() {
   return (
     <Layout>
       <div>home</div>
+      <Images />
     </Layout>
   );
 }
