7e2a65b3fa9d01bb97248c9e8446790da71209c0
Daniel <itzdanielmichael@gmail.com>
2022-10-04 13:32:50 -0400

    create about page

--- tutorial-recipes/src/pages/index.js
+++ tutorial-recipes/src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Layout from '../layouts/Layout/Layout';
-import Images from './components/images';
 
 export default function Home() {
   return (
     <Layout>
       <div>home</div>
-      <Images />
     </Layout>
   );
 }
